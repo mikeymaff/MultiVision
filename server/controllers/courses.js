6c80863995a8ffe2e3373e1633383e0f9fdb66c1
Michael Maffattone <mikeymaff@gmail.com>
2014-09-03 12:34:25 -0400

    add course details page and cached those

--- server/controllers/courses.js
+++ server/controllers/courses.js
@@ -4,4 +4,10 @@ exports.getCourses = function(req, res) {
 	Course.find({}).exec(function(err, collection) {
 		res.send(collection);
 	})
+};
+
+exports.getCourseById = function(req, res) {
+	Course.findOne({_id:req.params.id}).exec(function(err, course) {
+		res.send(course);
+	})
 }
